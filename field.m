6dbf2ea870c0bcb4c81e986fc0e6d4f72db53685
Michael <82557151+MichaelLidd@users.noreply.github.com>
2021-12-06 11:45:50 +0800

    Add files via upload

---
+++ field.m
@@ -0,0 +1,7 @@
+%  Start up the Field simulation system
+%
+%  Version 1.0, April 2, 1998, JAJ
+
+path(path, '/Users/michaelliddelow/Documents/MATLAB/VeinTech/Field_II_Simulation/Field_II_ver_3_30_mac/cfm_image_example')
+
+field_init(0)
